8a4ce2778106c735004ad1b883747b0600e5ce78
EZPK <thom.bruyer@gmail.com>
2016-08-05 21:27:35 +0200

    polishing + day/night script

---
+++ Assets/Scripts/dayNight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dayNight : MonoBehaviour {
+
+    public float cycle = 0.5f;
+  
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(cycle, 0, 0, Space.World);
+	}
+}

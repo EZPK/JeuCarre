2daf9158af250311068f60d02475e26f682d7810
EZPK <thom.bruyer@gmail.com>
2016-08-06 01:09:44 +0200

    early Raycast

---
+++ Assets/Scripts/raycastForward.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycastForward : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        float theDistance;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) *1000;
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            theDistance = hit.distance;
+            print("Objet touché" + " " + hit.collider.gameObject.name + " " + "distance" + " " + hit.distance);
+        }
+	}
+}
